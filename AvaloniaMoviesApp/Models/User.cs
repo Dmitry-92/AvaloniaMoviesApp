be04827e39de6d8c8ff194eb490fc6593c707070
Dmitry-92 <s1ash020692@gmail.com>
2026-06-21 14:11:47 +0300

    Добавлено поле PasswordHash в модель User + миграция

--- AvaloniaMoviesApp/Models/User.cs
+++ AvaloniaMoviesApp/Models/User.cs
@@ -8,6 +8,8 @@ public class User
     public string Username { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     
+    public string PasswordHash { get; set; } = string.Empty; // ← новое поле (храним хеш)
+    
     //Навигационное свойство: отзывы пользователя
     public List<Review> Reviews { get; set; } = new();
 }
